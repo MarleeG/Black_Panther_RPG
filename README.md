7051852c4ebb5cfa8747b8a8a9a621a1a0db0b60
Christina Gerard <ChristinaGerard@Christinas-MacBook-Pro.local>
2019-03-02 17:20:51 -0500

    added popovers to characters and readme is drafted

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Black_Panther_RPG
 
 ## Under Construction
+
+
+- Popper.js(https://popper.js.org/)
+- jQuery
+- 
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,39 +1,70 @@
 const log = console.log;
 
 function blackPanther() {
-    log('ready!');
 
     const characterImages = [
         {
             src: './assets/images/black_panther.png',
-            alt: 'black panther'
+            alt: 'black_panther',
+            name: 'Black Panther',
+            initialPoints: 210
         }, {
             src: './assets/images/killmonger.png',
-            alt: 'killmonger'
+            alt: 'killmonger',
+            name: 'Killmonger',
+            initialPoints: 200
         }, {
             src: './assets/images/klaw.png',
-            alt: 'klaw'
+            alt: 'klaw',
+            name: 'Klaw',
+            initialPoints: 198
         }, {
             src: './assets/images/man_ape.png',
-            alt: 'man ape'
+            alt: 'man_ape',
+            name: 'Man Ape',
+            initialPoints: 215
         }
     ];
 
+
+    $('.popover-dismiss').popover({
+        trigger: 'focus'
+    });
+
     function displayCharacters(array) {
-        array.forEach((element, i) => {
+
+
+        array.forEach(element => {
+
+            // Character Image
             var image = $('<img>');
             image.attr('class', 'character');
+            image.attr('id', element.alt);
             image.attr('src', element.src);
             image.attr('alt', element.alt);
-            $('.characters').append(image);
+
+            // adding popover to image
+            var span = $('<span>');
+            span.attr('data-container', 'body');
+            span.attr('id', 'span_' + element.alt);
+            span.attr('data-trigger', 'hover');
+            span.attr('data-toggle', 'popover');
+            span.attr('data-placement', 'bottom');
+            span.attr('data-content', element.name);
+
+            $('.characters').append(span.append(image));
         });
     }
-    
-    displayCharacters(characterImages)
+
+    function displayInstructions(instructions) {
+        $('.user_instructions').text(instructions);
+    }
 
 
+    displayInstructions('Choose your character');
+    displayCharacters(characterImages);
 
-
+    $('[data-toggle="popover"]').popover();
 }
 
 
